476538e3499b7d1d76dfbeffa379f6dcebe68407
James Cook <jcook.rubyist@gmail.com>
2015-02-18 07:21:40 -0600

    Make tests pass for other people

--- spec/ciso_spec.rb
+++ spec/ciso_spec.rb
@@ -1,17 +1,18 @@
 require_relative '../lib/ciso.rb'
+require 'open-uri'
 
 describe CISO do
   describe "compression" do
+    # TODO Write this out to a file after the first fetch
+    subject { CISO::Deflate.new(open('http://www.memtest.org/download/5.01/memtest86+-5.01.iso.gz')) }
 
+    before do
+      subject.deflate
+    end
+
+    it "can compress an iso" do
+      expect(subject).to be_compressed
+    end
   end
 
-  subject { CISO::Deflate.new(File.expand_path("~/Desktop/memtest86+-5.01.iso")) }
-
-  before do
-    subject.deflate
-  end
-
-  it "can compress an iso" do
-    expect(subject).to be_compressed
-  end
 end
